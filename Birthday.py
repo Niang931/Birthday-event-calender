c2da09028c71348f9f8427b30bdb7552eb48158c
Niang931 <kytnlmw1226@gmail.com>
2026-06-03 13:05:13 +0800

    Create class for birthday event

--- Birthday.py
+++ Birthday.py
@@ -8,23 +8,23 @@ import datetime
 
 class BirthdayEvent:
 
-    def __init__(self, name, date):
+    def __init__(self, name, date, end_date):
         self.name = name
         self.date = date
+        self.end_date = end_date
 
     def __str__(self):
         return f'{self.name} {self.date}'
 
-    def to_dict(self):
-        return {
-            'name':self.name,
-            'date':self.date
-        }
 
 class BirthdayCollection:
 
     def __init__(self):
         self.collection = []
+
+    def __iter__(self):
+        for bd in self.collection:
+            yield bd
 
     def load_birthdays(self):
         try:
@@ -35,8 +35,9 @@ class BirthdayCollection:
                     date_item = line.split()[:2]
                     date_str = ' '.join(date_item) + ' ' + CURRENT_YEAR
                     date_obj = datetime.datetime.strptime(date_str, "%b %d %Y")
+                    end_date = date_obj + datetime.timedelta(days=1)
                     name = ' '.join(line.split()[2:])
-                    birthday = BirthdayEvent(name, date_obj)
+                    birthday = BirthdayEvent(name, date_obj, end_date)
                     self.collection.append(birthday)
         except FileNotFoundError:
             self.collection = []
